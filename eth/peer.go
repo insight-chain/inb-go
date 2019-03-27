c84d0f771a60b56591b92519f845a21b5565642f
shenshuhangatinb <shenshuhang@insightchain.io>
2019-03-27 19:41:50 +0800

    peer

--- eth/peer.go
+++ eth/peer.go
@@ -329,38 +329,38 @@ func (p *peer) RequestReceipts(hashes []common.Hash) error {
 
 // Handshake executes the eth protocol handshake, negotiating version number,
 // network IDs, difficulties, head and genesis blocks.
-//func (p *peer) Handshake(network uint64, td *big.Int, head common.Hash, genesis common.Hash) error {
-//	// Send out own handshake in a new thread
-//	errc := make(chan error, 2)
-//	var status statusData // safe to read after two values have been received from errc
-//
-//	go func() {
-//		errc <- p2p.Send(p.rw, StatusMsg, &statusData{
-//			ProtocolVersion: uint32(p.version),
-//			NetworkId:       network,
-//			TD:              td,
-//			CurrentBlock:    head,
-//			GenesisBlock:    genesis,
-//		})
-//	}()
-//	go func() {
-//		errc <- p.readStatus(network, &status, genesis)
-//	}()
-//	timeout := time.NewTimer(handshakeTimeout)
-//	defer timeout.Stop()
-//	for i := 0; i < 2; i++ {
-//		select {
-//		case err := <-errc:
-//			if err != nil {
-//				return err
-//			}
-//		case <-timeout.C:
-//			return p2p.DiscReadTimeout
-//		}
-//	}
-//	p.td, p.head = status.TD, status.CurrentBlock
-//	return nil
-//}
+func (p *peer) Handshake(network uint64, td *big.Int, head common.Hash, genesis common.Hash) error {
+	// Send out own handshake in a new thread
+	errc := make(chan error, 2)
+	var status statusData // safe to read after two values have been received from errc
+
+	go func() {
+		errc <- p2p.Send(p.rw, StatusMsg, &statusData{
+			ProtocolVersion: uint32(p.version),
+			NetworkId:       network,
+			TD:              td,
+			CurrentBlock:    head,
+			GenesisBlock:    genesis,
+		})
+	}()
+	go func() {
+		errc <- p.readStatus(network, &status, genesis)
+	}()
+	timeout := time.NewTimer(handshakeTimeout)
+	defer timeout.Stop()
+	for i := 0; i < 2; i++ {
+		select {
+		case err := <-errc:
+			if err != nil {
+				return err
+			}
+		case <-timeout.C:
+			return p2p.DiscReadTimeout
+		}
+	}
+	p.td, p.head = status.TD, status.CurrentBlock
+	return nil
+}
 
 func (p *peer) readStatus(network uint64, status *statusData, genesis common.Hash) (err error) {
 	msg, err := p.rw.ReadMsg()
