ce53efba72f3c1ae9e6f4e7fd7e8e770415038f0
xinleiatinb <mengxinlei@insightchain.io>
2019-05-20 19:22:58 +0800

    Increase, decrease, and so on of CPU and net

---
+++ core/state/privileged_state_object.go
@@ -0,0 +1,273 @@
+package state
+
+import (
+	"errors"
+	"math/big"
+	"strconv"
+	"time"
+)
+
+type MortgageManager interface {
+
+	afterMortgageCpu(mortgagtionNumber *big.Int) error
+	afterMortgageNet(mortgagtionNumber *big.Int) error
+	afterUnmortgageCpu(unMortgagtionNumber *big.Int) error
+	afterUnmortgageNet(unMortgagtionNumber *big.Int) error
+
+}
+
+type manager struct {
+
+}
+
+func (a *manager)afterMortgageCpu(mortgagtionNumber *big.Int) error {
+	PrivilegedSateObject.AddCpu(mortgagtionNumber)
+	return errors.New("")
+}
+func (a *manager)afterMortgageNet(mortgagtionNumber *big.Int) error {
+	PrivilegedSateObject.AddCpu(mortgagtionNumber)
+	return errors.New("")
+}
+func (a *manager)afterUnMortgageCpu(mortgagtionNumber *big.Int) error {
+	PrivilegedSateObject.SubNet(mortgagtionNumber)
+	return errors.New("")
+}
+func (a *manager)afterUnMortgageNet(mortgagtionNumber *big.Int) error {
+	PrivilegedSateObject.SubNet(mortgagtionNumber)
+	return errors.New("")
+}
+//User's mortgage and unmortgage operations
+//mortgage
+func (self *stateObject)MortgageCpu(amount *big.Int)  {
+	//self.updateAccountCpuAndNet()
+	if (amount.Cmp(big.NewInt(0)) == 0) || (amount.Cmp(self.data.Balance) == 1){
+		return
+	}
+
+	//Add CPU and net to the account
+	//self.SubBalance(amount)
+	self.AddCpu(amount)
+
+	self.db.Finalise(false)
+	root := self.db.IntermediateRoot(false)
+	self.db.Commit(false)
+	self.db.Database().TrieDB().Commit(root, true)
+}
+func (self *stateObject)MortgageNet(amount *big.Int)  {
+
+	self.updateAccountCpuAndNet()
+	if (amount.Cmp(big.NewInt(0)) == 0) || (amount.Cmp(self.data.Balance) == 1){
+		return
+	}
+
+	//Add CPU and net to the account
+	//self.SubBalance(amount)
+	self.AddNet(amount)
+
+	self.db.Finalise(true)
+	root := self.db.IntermediateRoot(false)
+	self.db.Commit(false)
+	self.db.Database().TrieDB().Commit(root, true)
+}
+//UnMortgage
+func (self *stateObject)UnMortgageCpu(amount *big.Int) {
+
+	usableCpu := self.data.Resources.CPU.Usableness;
+	usableNet := self.data.Resources.NET.Usableness;
+
+	self.updateAccountCpuAndNet()
+	if (amount.Cmp(big.NewInt(0)) == 0) || (amount.Cmp(self.data.Resources.CPU.MortgagteINB) == 1){
+		return
+	}
+	if amount.Cmp(self.data.Resources.CPU.MortgagteINB)==0{
+		self.setCpu(big.NewInt(0),big.NewInt(0),big.NewInt(0))
+		self.AddBalance(amount)
+	}else {
+		//You need to convert number to the type of big
+		expendCpuFromUnMortgageCpu := big.NewInt(50)
+		expendNetFromUnMortgageNet := big.NewInt(300)
+
+		if expendNetFromUnMortgageNet.Cmp(usableNet) == 1{
+			return
+		}
+		if expendCpuFromUnMortgageCpu.Cmp(usableCpu.Add(usableCpu,amount.Div(amount,PerINBGainCpuNum))) == 1{
+			return
+		}
+
+		//self.AddBalance(amount)
+		self.SubCpu(amount)
+	}
+}
+func (self *stateObject)UnMortgageNet(amount *big.Int) {
+
+	usableCpu := self.data.Resources.CPU.Usableness;
+	usableNet := self.data.Resources.NET.Usableness;
+
+	self.updateAccountCpuAndNet()
+	if (amount.Cmp(big.NewInt(0)) == 0) || (amount.Cmp(self.data.Resources.NET.MortgagteINB) == 1){
+		return
+	}
+	if amount.Cmp(self.data.Resources.NET.MortgagteINB)==0{
+		self.setNet(big.NewInt(0),big.NewInt(0),big.NewInt(0))
+		self.AddBalance(amount)
+	}else {
+		//You need to convert number to the type of big
+		expendCpuFromUnMortgageCpu := big.NewInt(50)
+		expendNetFromUnMortgageNet := big.NewInt(300)
+
+		if expendCpuFromUnMortgageCpu.Cmp(usableCpu) == 1{
+			return
+		}
+		if expendNetFromUnMortgageNet.Cmp(usableNet.Add(usableNet,amount.Div(amount,PerINBGainNetNum))) == 1{
+			return
+		}
+
+		//self.AddBalance(amount)
+		self.SubNet(amount)
+	}
+}
+//Increase or decrease the user's CPU or net
+//Mortgage
+func (c *stateObject) AddCpu(amount *big.Int){
+	//c.AddMortgageINBOfCpu(amount)
+	gainNumberOfCpu := c.db.GainNumberOfCpu(amount)
+	used := c.data.Resources.CPU.Used
+	usable := c.AddUsableCpu(gainNumberOfCpu)
+	mortgagetion := c.AddMortgageINBOfCpu(amount)
+	c.SetCpu(used,usable,mortgagetion)
+
+	c.db.GetPrivilegedSateObject()
+	PrivilegedSateObject.AddMortgageINBOfCpu(amount)
+}
+func (c *stateObject) AddNet(amount *big.Int){
+
+	gainNumberOfNet := c.db.GainNumberOfNet(amount)
+	used := c.data.Resources.NET.Used
+	usable := c.AddUsableNet(gainNumberOfNet)
+	mortgagetion := c.AddMortgageINBOfNet(amount)
+	c.setNet(used,usable,mortgagetion)
+
+	c.db.GetPrivilegedSateObject()
+	PrivilegedSateObject.AddMortgageINBOfNet(amount)
+}
+//unMorgage
+func (c *stateObject) SubCpu(amount *big.Int){
+	expendCpuFromUnMortgageCpu := big.NewInt(50)
+	expendNetFromUnMortgageNet := big.NewInt(300)
+
+	used := c.AddUsableCpu(expendCpuFromUnMortgageCpu)
+	mortgagetion := c.SubMortgageINBOfCpu(amount)
+	calculateCpuNumber := c.db.GainNumberOfCpu(c.data.Resources.CPU.MortgagteINB)
+	remainingCpuNumber := calculateCpuNumber.Sub(calculateCpuNumber,expendCpuFromUnMortgageCpu)
+	usable := c.AddUsableCpu(remainingCpuNumber)
+	c.SetCpu(used,usable,mortgagetion)
+
+	netUsed := c.AddUsedNet(expendNetFromUnMortgageNet)
+	netUsable := c.SubUsableNet(expendNetFromUnMortgageNet)
+	c.SetNet(netUsed,netUsable,c.data.Resources.NET.MortgagteINB)
+
+	c.db.GetPrivilegedSateObject()
+	PrivilegedSateObject.SubMortgageINBOfCpu(amount)
+}
+func (c *stateObject) SubNet(amount *big.Int){
+	expendCpuFromUnMortgageCpu := big.NewInt(50)
+	expendNetFromUnMortgageNet := big.NewInt(300)
+
+	used := c.AddUsableNet(expendNetFromUnMortgageNet)
+	mortgagetion := c.SubMortgageINBOfNet(amount)
+	calculateNetNumber := c.db.GainNumberOfNet(c.data.Resources.NET.MortgagteINB)
+	remainingNetNumber := calculateNetNumber.Sub(calculateNetNumber,expendNetFromUnMortgageNet)
+	usable := c.AddUsableNet(remainingNetNumber)
+	c.SetNet(used,usable,mortgagetion)
+
+	cpuUsed := c.AddUsedCpu(expendCpuFromUnMortgageCpu)
+	cpuUsable := c.SubUsableCpu(expendCpuFromUnMortgageCpu)
+	c.SetCpu(cpuUsed,cpuUsable,c.data.Resources.CPU.MortgagteINB)
+
+	c.db.GetPrivilegedSateObject()
+	PrivilegedSateObject.SubMortgageINBOfNet(amount)
+}
+//Set up the user's CPU and net
+func (self *stateObject) SetCpu(usedAmount *big.Int,usableAmount *big.Int, mortgageInb *big.Int) {
+
+	self.db.journal.append(cpuChange{
+		account: &self.address,
+		Used: new(big.Int).Set(self.data.Resources.CPU.Used),
+		Usableness:new(big.Int).Set(self.data.Resources.CPU.Usableness),
+		MortgagteINB:new(big.Int).Set(self.data.Resources.CPU.MortgagteINB),
+	})
+	self.setCpu(usedAmount,usableAmount,mortgageInb)
+}
+func (self *stateObject) setCpu(usedAmount *big.Int,usableAmount *big.Int, mortgageInb *big.Int) {
+
+	self.data.Resources.CPU.Used = usedAmount
+	self.data.Resources.CPU.Usableness = usableAmount
+	self.data.Resources.CPU.MortgagteINB = mortgageInb
+}
+func (self *stateObject) SetNet(usedAmount *big.Int,usableAmount *big.Int, mortgageInb *big.Int) {
+
+	self.db.journal.append(netChange{
+		account: &self.address,
+		Used: new(big.Int).Set(self.data.Resources.NET.Used),
+		Usableness:new(big.Int).Set(self.data.Resources.NET.Usableness),
+		MortgagteINB:new(big.Int).Set(self.data.Resources.NET.MortgagteINB),
+	})
+	self.setNet(usedAmount,usableAmount,mortgageInb)
+}
+func (self *stateObject) setNet(usedAmount *big.Int,usableAmount *big.Int, mortgageInb *big.Int) {
+	self.data.Resources.NET.Used = usedAmount
+	self.data.Resources.NET.Usableness = usableAmount
+	self.data.Resources.NET.MortgagteINB = mortgageInb
+}
+//Usable, usable, and mortgage are used in the CPU or Net
+//used
+func (self *stateObject) AddUsedCpu(amout *big.Int) *big.Int{
+	return self.data.Resources.CPU.Used.Add(self.data.Resources.CPU.Used,amout)
+}
+func (self *stateObject) AddUsedNet(amout *big.Int) *big.Int{
+	return self.data.Resources.NET.Used.Add(self.data.Resources.NET.Used,amout)
+}
+//Usable
+func (self *stateObject) AddUsableCpu(amout *big.Int) *big.Int{
+	return self.data.Resources.CPU.Usableness.Add(self.data.Resources.CPU.Usableness,amout)
+}
+func (self *stateObject) AddUsableNet(amout *big.Int) *big.Int{
+	return self.data.Resources.NET.Usableness.Add(self.data.Resources.NET.Usableness,amout)
+}
+func (self *stateObject) SubUsableCpu(amout *big.Int) *big.Int{
+	return self.data.Resources.CPU.Usableness.Sub(self.data.Resources.CPU.Usableness,amout)
+}
+func (self *stateObject) SubUsableNet(amout *big.Int) *big.Int{
+	return self.data.Resources.NET.Usableness.Sub(self.data.Resources.NET.Usableness,amout)
+}
+//Mortgage
+func (self *stateObject) AddMortgageINBOfCpu(amout *big.Int) *big.Int{
+	return self.data.Resources.CPU.MortgagteINB.Add(self.data.Resources.CPU.MortgagteINB,amout)
+}
+func (self *stateObject) AddMortgageINBOfNet(amout *big.Int) *big.Int{
+	return self.data.Resources.NET.MortgagteINB.Add(self.data.Resources.NET.MortgagteINB,amout)
+}
+func (self *stateObject) SubMortgageINBOfCpu(amout *big.Int) *big.Int{
+	return self.data.Resources.CPU.MortgagteINB.Sub(self.data.Resources.CPU.MortgagteINB,amout)
+}
+func (self *stateObject) SubMortgageINBOfNet(amout *big.Int) *big.Int{
+	return self.data.Resources.NET.MortgagteINB.Sub(self.data.Resources.NET.MortgagteINB,amout)
+}
+//Updates the user's CPU and net 24 hours a day
+func (self *stateObject) updateAccountCpuAndNet (){
+	if  len(self.data.Resources.Date) !=0 {
+		list,_ := strconv.ParseInt(self.data.Resources.Date, 10, 64)
+		now := time.Now().Unix()
+		subM := now - list
+		if subM >= 24*3600 {
+			//Update the CPU and Net owned by the user
+			self.SetCpu(big.NewInt(0),self.db.GainNumberOfCpu(self.data.Resources.NET.MortgagteINB),self.data.Resources.NET.MortgagteINB)
+			self.SetNet(big.NewInt(0),self.db.GainNumberOfNet(self.data.Resources.CPU.MortgagteINB),self.data.Resources.NET.MortgagteINB)
+		}
+		self.data.Resources.Date = strconv.FormatInt(now,10)
+	}else {
+		now := time.Now().Unix()
+		self.data.Resources.Date = strconv.FormatInt(now,10)
+	}
+
+}
---
+++ core/state/resource.go
@@ -0,0 +1,106 @@
+package state
+
+import (
+	"math/big"
+	"strconv"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	PerINBGainNetNum *big.Int
+	PerINBGainCpuNum *big.Int
+)
+
+type record struct {
+	rddData RdDdataInterface
+	hash atomic.Value
+	size atomic.Value
+	from atomic.Value
+}
+type RdDdataInterface interface {
+
+}
+type BaseRecordData struct {
+	AccountNonce    uint64                 `json:"nonce"        gencodec:"required"`
+	CpuPrice        *big.Int               `json:"netPrice"     gencodec:"required"`
+	NetPrice        *big.Int               `json:"cpuPrice"     gencodec:"required"`
+	Amount          *big.Int               `json:"value"        gencodec:"required"`  //inb数量
+	CpuAmount       *big.Int               `json:"CpuAmount"    gencodec:"required"`  //cpu的数量
+	NetAmount       *big.Int               `json:"NetAmount"    gencodec:"required"`  //net的数量
+	//签名值
+	V *big.Int `json:"v" gencodec:"required"`
+	R *big.Int `json:"r" gencodec:"required"`
+	S *big.Int `json:"s" gencodec:"required"`
+}
+type Mortgagtion struct{
+	BaseRecordData
+	mortgagtionDate  time.Time
+}
+type Unmortgagtion struct{
+	BaseRecordData
+	UnmortgagtionDate  time.Time
+	expectedDate    time.Time
+}
+
+//1 inb can be exchanged for Net
+func (c *StateDB)PerInbIsNet(){
+
+	as :=  PrivilegedSateObject.data.Resources.CPU.MortgagteINB
+	asString := as.Set(as).String()
+	asValue, err := strconv.ParseInt(asString, 10, 64)
+	if err!=nil {
+
+	}
+
+	if as == big.NewInt(0)||asValue == 0 {
+		PerINBGainNetNum = big.NewInt(1).Div(big.NewInt(86400*1000*1024*1000),big.NewInt(10*100000000))
+	}else {
+		PerINBGainNetNum = as.Div(big.NewInt(86400*1000*1024*1000),as)
+	}
+}
+//1 inb can be exchanged for CPU
+func (c *StateDB)PerInbIsCpu() {
+
+	as :=  PrivilegedSateObject.data.Resources.CPU.MortgagteINB
+	asString := as.Set(as).String()
+	asValue, err := strconv.ParseInt(asString, 10, 64)
+	if err!=nil {
+
+	}
+
+	if as == big.NewInt(0) ||asValue == 0{
+		PerINBGainCpuNum = big.NewInt(1).Div(big.NewInt(691200000*1000),big.NewInt(10*100000000))
+	}else {
+		PerINBGainCpuNum = as.Div(big.NewInt(691200000*1000),as)
+	}
+}
+//mortgage how much NET inb gets
+func (c *StateDB)GainNumberOfNet(inbNumber *big.Int)(*big.Int) {
+
+	c.PerInbIsNet()
+
+	if PerINBGainNetNum == big.NewInt(0) {
+		return big.NewInt(0)
+	}else {
+		if inbNumber != nil {
+			return inbNumber.Mul(inbNumber,PerINBGainNetNum)
+		}
+		return big.NewInt(0)
+	}
+}
+//mortgage how much CPU inb gets
+func (c *StateDB)GainNumberOfCpu(inbNumber *big.Int)(*big.Int) {
+
+	c.PerInbIsCpu()
+	if PerINBGainCpuNum == big.NewInt(0) {
+		return big.NewInt(0)
+	}else {
+		if inbNumber != nil {
+
+			//return inbNumber.Mul(inbNumber,PerINBGainCpuNum)
+			return inbNumber.Mul(inbNumber,big.NewInt(10))
+		}
+		return big.NewInt(0)
+	}
+}
