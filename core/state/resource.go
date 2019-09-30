789f0fc865a023547b6be13a65c89803396eb9a7
insightchain <insight@insightchain.io>
2019-09-30 21:07:33 +0800

    add staking total circulation limit

--- core/state/resource.go
+++ core/state/resource.go
@@ -75,7 +75,9 @@ func (c *StateDB) UnitConvertNet() *big.Int {
 	if mortgage.Cmp(params.TxConfig.MortgageInbLimit) < 0 {
 		totalMortgageInb = params.TxConfig.MortgageInbLimit
 	}
-
+	if mortgage.Cmp(big.NewInt(1e+10)) > 0 {
+		totalMortgageInb = big.NewInt(1e+10)
+	}
 	weiToNet := big.NewInt(1).Div(params.TxConfig.Net, totalMortgageInb)
 	temp := big.NewInt(1).Div(params.TxConfig.Wei, params.TxConfig.WeiOfUseNet)
 	weiToNet.Div(weiToNet, temp)
