7e146c24448f66a2ea0ec47a8c437dc90073fa62
keien <wangenkai@insightchain.io>
2019-12-10 10:12:20 +0800

    change go-ethereum to inb-go

--- cmd/ginb/main.go
+++ cmd/ginb/main.go
@@ -1,18 +1,18 @@
-// Copyright 2014 The go-ethereum Authors
-// This file is part of go-ethereum.
+// Copyright 2014 The inb-go Authors
+// This file is part of inb-go.
 //
-// go-ethereum is free software: you can redistribute it and/or modify
+// inb-go is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// go-ethereum is distributed in the hope that it will be useful,
+// inb-go is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+// along with inb-go. If not, see <http://www.gnu.org/licenses/>.
 
 // ginb is the official command-line client for Ethereum.
 package main
@@ -49,7 +49,7 @@ var (
 	// Git SHA1 commit hash of the release (set via linker flags)
 	gitCommit = ""
 	// The app that holds all commands and flags.
-	app = utils.NewApp(gitCommit, "the go-ethereum command line interface")
+	app = utils.NewApp(gitCommit, "the inb-go command line interface")
 	// flags that configure the node
 	nodeFlags = []cli.Flag{
 		utils.IdentityFlag,
