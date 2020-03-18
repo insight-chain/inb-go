ce256832894c762948d7a97970a251900f6d1522
shenshuhangatinb <shenshuhang@insightchain.io>
2020-03-18 16:23:18 +0800

    debug test4

--- core/evm.go
+++ core/evm.go
@@ -26,7 +26,8 @@ import (
 	"math/big"
 )
 
-//test3
+// ChainContext supports retrieving headers and consensus parameters from the
+// current blockchain to be used during transaction processing.
 type ChainContext interface {
 	// Engine retrieves the chain's consensus engine.
 	Engine() consensus.Engine
